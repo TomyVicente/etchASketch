97a3e96a0479560424b2893a215886bffe5bfafa
Tomas Ignacio Vicente <tomasdf053@gmail.com>
2023-05-08 19:38:22 -0300

    Change all the user interface with CSS and the HTML structure

--- code.js
+++ code.js
@@ -5,18 +5,11 @@
 // Set a hoover effect so when the mouse pass arround the divs will change the color
 //Add a button to the top of the page that will send a popup asking for how many squares the user wants while is less than 100
 
+const gridContainerClass = document.getElementsByClassName("grid-container");
+const gridContainer = gridContainerClass[0];
 
-let gridQuantity = 16;
-let classContainer = document.getElementsByClassName("container");
-let gridContainer = classContainer [0];
+gridContainer.addEventListener("hoover", );
 
-//For cycle to create a N x N grid
-for(let i =0 ;i<gridQuantity;i++){
-    for (let a = 0; a < gridQuantity; a++) {
-        let div = document.createElement('div');
-        div.className = "grid";
-        gridContainer.appendChild(div);
-    }
-
+function createGrid(){
+    
 }
-
